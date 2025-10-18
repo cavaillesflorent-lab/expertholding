a5d882be2e5d0e66e473dae4d06b275fcc5c56b3
Cavailles <florent@MacBook-Air-de-Cavailles.local>
2025-10-18 14:35:18 +0200

    Disable ESLint during production build

--- next.config.js
+++ next.config.js
@@ -1,10 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
   images: {
     domains: ['images.unsplash.com'],
     formats: ['image/avif', 'image/webp'],
   },
-  swcMinify: true,
   reactStrictMode: true,
   compress: true,
 }
